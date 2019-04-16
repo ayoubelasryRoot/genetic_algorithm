7c58ab626304309af206edc8b9827e7a199c755a
ayoub <ayoub.elasry96@gmail.com>
2019-04-16 15:45:38 -0400

    refactoring

--- algo.py
+++ algo.py
@@ -1,14 +1,9 @@
-from multiprocessing import Process, Lock
-
 import numpy as np
 from load_data import load_data_form_file
 import sys
 import time
 import random
 np.set_printoptions(linewidth=np.inf)
-
-best_cost_greedy = sys.maxsize * -1
-best_model_greedy = 0
 
 
 class LegoInformation:
@@ -70,15 +65,12 @@ def genetic_algorithm(lego_information=LegoInformation, start=time.time()):
     # to compare models
     best_solution_models = None
     best_solution_price = -1 * sys.maxsize
-    nb_species_by_iteration = 100
+    nb_species_by_iteration = 75
     # crossover
     parent_a = -1
     parent_b = -1
     previous_parent_a_cost = 0
     mutation_probability = 0
-
-    global best_model_greedy
-    global best_cost_greedy
 
     population_models = np.zeros((nb_species_by_iteration, lego_information.nb_models))
     population_models_cost = np.zeros(nb_species_by_iteration)
@@ -95,45 +87,14 @@ def genetic_algorithm(lego_information=LegoInformation, start=time.time()):
 
             models_used_by_generation = np.zeros(lego_information.nb_models).astype("int32")
             if parent_a != -1:
-                for h in range(0, lego_information.nb_models):
-                    value_to_set = population_models[parent_a][h]
-                    if population_models[parent_b][h] < population_models[parent_a][h]:
-                        value_to_set = population_models[parent_b][h]
-                    if value_to_set > 0:
-                        models_used_by_generation[h] = value_to_set
-
-                # mutation
-                if np.random.random() < mutation_probability:
-                    random_index_for_mutation = random.choice([
-                        index for index in range(0, lego_info.nb_models) if models_used_by_generation[index] > 0
-                    ])
-                    models_used_by_generation[random_index_for_mutation] -= 1
+                species_selection(lego_information, population_models, parent_a, parent_b, models_used_by_generation, mutation_probability)
 
             updated_legos = np.copy(lego_information.initial_lego)
-            for model_index in range(0, lego_information.nb_models):
-                if models_used_by_generation[model_index] > 0:
-                    updated_legos -= lego_information.lego_models[model_index] * models_used_by_generation[model_index]
-
+            updated_current_lego(lego_information, models_used_by_generation, updated_legos)
             while not current_lego_done(updated_legos):
-                if not_negative_value(updated_legos) and np.random.random() < 0.9:
-                    lock = Lock()
-                    processes = []
-                    for i in range(0, lego_information.nb_models):
-                        p = Process(target=greedy_algorithm, args=(lock, updated_legos, lego_information, i))
-                        processes.append(p)
-                        p.start()
-
-                    for process in processes:
-                        process.join()
-
-                    updated_legos -= lego_information.lego_models[best_model_greedy]
-                    models_used_by_generation[best_model_greedy] += 1
-                    best_cost_greedy = sys.maxsize * -1
-                    best_model_greedy = 0
-                else:
-                    model_index = random_valid_index_model(lego_information=lego_information, current_lego=updated_legos)
-                    updated_legos = np.subtract(updated_legos, lego_information.lego_models[model_index])
-                    models_used_by_generation[model_index] += 1
+                model_index = random_valid_index_model(lego_information=lego_information, current_lego=updated_legos)
+                updated_legos = np.subtract(updated_legos, lego_information.lego_models[model_index])
+                models_used_by_generation[model_index] += 1
 
             cost_generation = np.dot(updated_legos, lego_information.lego_price)
             if cost_generation > best_solution_price:
@@ -153,7 +114,28 @@ def genetic_algorithm(lego_information=LegoInformation, start=time.time()):
         previous_parent_a_cost = population_models_cost[parent_a]
         population_models_cost[parent_a] = sys.maxsize * -1
         parent_b = np.argmax(population_models_cost)
-        # population_models_cost[parent_a] = original_value
+
+
+def species_selection(lego_information, population_models, parent_a, parent_b, models_used_by_generation, mutation_probability):
+    for h in range(0, lego_information.nb_models):
+        value_to_set = population_models[parent_a][h]
+        if population_models[parent_b][h] < population_models[parent_a][h]:
+            value_to_set = population_models[parent_b][h]
+        if value_to_set > 0:
+            models_used_by_generation[h] = value_to_set
+
+    # mutation
+    if np.random.random() < mutation_probability:
+        random_index_for_mutation = random.choice([
+            index for index in range(0, lego_info.nb_models) if models_used_by_generation[index] > 0
+        ])
+        models_used_by_generation[random_index_for_mutation] -= 1
+
+
+def updated_current_lego(lego_information, models_used_by_generation, updated_legos):
+    for model_index in range(0, lego_information.nb_models):
+        if models_used_by_generation[model_index] > 0:
+            updated_legos -= lego_information.lego_models[model_index] * models_used_by_generation[model_index]
 
 
 def not_negative_value(current_lego):
